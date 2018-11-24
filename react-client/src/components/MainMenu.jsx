3004e1437e18237745f11ba28b9ff5c53135d926
Ashraf Hasan Alsheikh <alsheikh813@gmail.com>
2018-11-24 20:02:19 +0200

    project Skeleton 01

--- react-client/src/components/MainMenu.jsx
+++ react-client/src/components/MainMenu.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
 const MainMenu = (props) => (
-  <h1>
-    Home | About | List Item | Add Item | Search for Item | Login/Signup
-  </h1>
+  <div>
+	  <div><h1>Home | About | List Item | Add Item | Search for Item | Login | Signup</h1></div>
+	  <button className="btn" onClick={function(){console.log("home!")}}>Home </button>
+	  <button className="btn" onClick={function(){console.log("About!")}}>About </button>
+	  <button className="btn" onClick={function(){console.log("List Item!")}}>List Item </button>
+	  <button className="btn" onClick={function(){console.log("Add Item!")}}>Add Item </button>
+	  <button className="btn" onClick={function(){console.log("Search for Item!")}}>Search for Item </button>
+	  <button className="btn" onClick={function(){console.log("Login!")}}>Login </button>
+	  <button className="btn" onClick={function(){console.log("Signup!")}}>Signup </button>
+	   
+	  
+  </div>
 )
 
 export default MainMenu;
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -8,8 +8,11 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-      items: []
+      items: [],
+      ren: 0,
     }
+
+    this.pageRender = this.pageRender.bind(this);
   }
 
   componentDidMount() {
@@ -17,7 +20,8 @@ class App extends React.Component {
       url: '/items', 
       success: (data) => {
         this.setState({
-          items: data
+          items: data,
+          
         })
       },
       error: (err) => {
@@ -26,9 +30,24 @@ class App extends React.Component {
     });
   }
 
+  pageRender() {
+    console.log("pageID", 1)
+   
+  }
+
   render () {
     return (<div>
-      <MainMenu />
+      
+      <div>
+        <div><h1>Home | About | List Item | Add Item | Search for Item | Login | Signup</h1></div>
+        <button className="btn" onClick={function(){this.setState({ren: 2}); console.log("home!", this.ren)}}>Home </button>
+        <button className="btn" onClick={function(){this.pageRender}}>About </button>
+        <button className="btn" onClick={function(){console.log("List Item!")}}>List Item </button>
+        <button className="btn" onClick={function(){console.log("Add Item!")}}>Add Item </button>
+        <button className="btn" onClick={function(){console.log("Search for Item!")}}>Search for Item </button>
+        <button className="btn" onClick={function(){console.log("Login!")}}>Login </button>
+        <button className="btn" onClick={function(){console.log("Signup!")}}>Signup </button>  
+      </div>
       <h1>Item List</h1>
       <List items={this.state.items}/>
     </div>)
